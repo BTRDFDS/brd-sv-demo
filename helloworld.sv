1351b78cb2302801d311c6786309a9820848fd75
Bi RuiDe <1774627100@qq.com>
2026-09-22 00:32:13 +0000

    clk

--- helloworld.sv
+++ helloworld.sv
@@ -11,7 +11,13 @@ module top ();
 		#10 $display("%t hello world",$time);
 		#10 $display("%t %d",$time,$random);
 		repeat(5) #10 $display("%t repeat 5",$time);
-		for(int i=0;i<5;i++)$display("%t for %d",$time,i); 
+		for(int i=0;i<5;i++)#3$display("%t for %d",$time,i); 
 		$finish();
 	end
+    logic clk=0;
+    initial forever #5 clk = ~clk;
+
+    always_ff@(posedge clk)begin
+		$display("%t clk time",$time);
+    end
 endmodule
