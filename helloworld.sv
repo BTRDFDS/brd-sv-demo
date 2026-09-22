1190d44a609eed7c8800df9a544d83dea62dbdfc
Bi RuiDe <1774627100@qq.com>
2026-09-22 00:23:52 +0000

    新增for

--- helloworld.sv
+++ helloworld.sv
@@ -1,10 +1,17 @@
+class HelloWorld;
+    function void print_hello();
+        $display("Hello World from a SystemVerilog Class!");
+    endfunction
+endclass
+
 module top ();
 	initial begin
-		$display("hello world");
-		#10 $display("hello world");
-		#10 $display("hello world");
-		#10 $display("hello world");
-		#10 $display("hello world");
+        HelloWorld hw = new();
+        hw.print_hello();
+		#10 $display("%t hello world",$time);
+		#10 $display("%t %d",$time,$random);
+		repeat(5) #10 $display("%t repeat 5",$time);
+		for(int i=0;i<5;i++)$display("%t for %d",$time,i); 
 		$finish();
 	end
 endmodule
--- makefile
+++ makefile
@@ -1,3 +1,3 @@
 hw:
-	verilator helloworld.sv --Mdir build/helloworld --binary --timing
+	verilator helloworld.sv --Mdir build/helloworld --binary --timing --timescale "1ms/1ms"
 	./build/helloworld/Vhelloworld
